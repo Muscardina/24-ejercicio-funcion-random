dc06c23199de02bf9a9f8cad53c84b730e9ed3c3
Muscardina <vergonzalezsainz@gmail.com>
2024-06-06 13:43:24 +0200

    Primer intento

---
+++ funcion javascript.js
@@ -0,0 +1,18 @@
+
+Math.floor(Math.random() * 10000);
+cliente1 = Math.floor(Math.random() * 10000);
+cliente2 = Math.floor(Math.random() * 10000);
+cliente3 = Math.floor(Math.random() * 10000);
+cliente4 = Math.floor(Math.random() * 10000);
+cliente5 = Math.floor(Math.random() * 10000);
+cliente6 = Math.floor(Math.random() * 10000);
+cliente7 = Math.floor(Math.random() * 10000);
+cliente8 = Math.floor(Math.random() * 10000);
+cliente9 = Math.floor(Math.random() * 10000);
+cliente10 = Math.floor(Math.random() * 10000);
+listaclientes = (cliente1, cliente2, cliente3, cliente4, cliente5, cliente6, cliente7, cliente8, cliente9, cliente10)
+
+
+
+console-log 
+
